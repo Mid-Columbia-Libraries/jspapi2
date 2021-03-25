c76f5405c1f0d92ea4c2beb505bfcb01c81ef65d
Vance <coletain@gmail.com>
2021-03-25 16:28:13 -0700

    Cleanup, removes webpack, shuffles and renames some stuff internally

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/scripts/PAPI.js',
+  input: 'src/scripts/jsPAPI.js',
   output: {
     name: 'jsPAPI',
     dir: 'dist',
