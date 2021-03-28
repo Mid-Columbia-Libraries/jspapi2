ead44ee2b72c650bf23249a10edb180265c93b9b
Vance <coletain@gmail.com>
2021-03-28 16:13:26 -0700

    adds bibParser util func

--- src/scripts/util/bibParser.js
+++ src/scripts/util/bibParser.js
@@ -1,7 +1,46 @@
+import _ from 'lodash';
 /**
  * Converts a set of bib record rows into an object that is easier to use
  * @param {Object} bib A bib record from Polaris API
  */
+ 
 export default function(bib) {
-  return {};
+  if (bib.PAPIErrorCode !== 0) return false;
+  const p2 = {};
+  bib.BibGetRows.forEach((v) => {
+    let id = v.ElementID;
+    if (!p2[id]) p2[id] = {
+      label: v.Label,
+      typeID: id,
+      type: lookupType(id),
+      values: [],
+    }
+    p2[id].values.push(v.Value);
+  });
+
+  return p2;
+}
+
+function lookupType(id) {
+  const types = {
+    2: 'publisher',
+    3: 'description',
+    5: 'edition',
+    6: 'isbn',
+    7: 'available',
+    8: 'holds',
+    9: 'summary',
+    13: 'cn',
+    16: 'systemIn',
+    17: 'format',
+    18: 'author',
+    20: 'subjects',
+    23: 'lccn',
+    28: 'notes',
+    35: 'title',
+    44: 'targetAudience',
+  }
+  let type = types[id];
+  if (!type) console.log(`Missing bib type ${id}`);
+  return (type) ? type : id;
 }
